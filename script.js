090b0b697c4ed6dbaaa36a8638016af3782c7e6e
Yarinjo <gladysyora@gmail.com>
2024-03-10 20:18:24 +0200

    added beta version of homework 16

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+    dots: true,
+    items: 1,
+
+})
